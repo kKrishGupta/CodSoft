c0d520089aa4db8b5a140cc4927cc684143eec41
kKrishGupta <kg20060220@gmail.com>
2025-06-30 18:31:15 +0530

    added commits

--- Landing Page/script.js
+++ Landing Page/script.js
@@ -1,7 +1,29 @@
-const toggleBtn = document.querySelector('.toggle-menu button i');
+const toggleBtn = document.querySelector('.toggle-menu button ');
 const navMenyy = document.querySelector('.nav-menu ul');
 
 
 toggleBtn.addEventListener('click', function(){
   navMenu.classList.toggle('show');
 });
+
+
+const darkmodetoggle = document.querySelector('.dark-mode-toggle');
+const body = document.querySelector('body');
+
+
+darkmodetoggle.addEventListener('click', function() {
+  body.classList.toggle('dark-mode');
+updateDarkmodeicon();
+});
+
+function updateDarkmodeicon(){
+
+  const darkmodeicon = darkmodetoggle.querySelector('i');
+if(body.classList.contains('dark-mode')){
+  darkmodeicon.classList.remove('fa-moon');
+  darkmodeicon.classList.add('fa-sun');
+}else{
+darkmodeicon.classList.add('fa-moon');
+darkmodeicon.classList.remove('fa-sun');
+}
+}
